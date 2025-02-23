514747acd8f0ff66d54e3d3f712319f998eb662b
wosimidwa <152811454+wosimidwa@users.noreply.github.com>
2025-02-24 02:22:33 +0300

    Create config.py

---
+++ Ön İşleme/config.py
@@ -0,0 +1,13 @@
+
+data_directory = "F:/iki/sadece_kriz/28_ch"
+output_directory = "F:/iki/tekod"
+annotations_file = "F:/iki/kriz_28.txt"
+
+ch_mapping = {
+    'FP1-F7': 'Fp1', 'F7-T7': 'F7', 'T7-P7': 'T7', 'P7-O1': 'P7', 
+    'F3-C3': 'F3', 'C3-P3': 'C3', 'P3-O1': 'P3', 
+    'FP2-F4': 'Fp2', 'F4-C4': 'F4', 'C4-P4': 'C4', 'P4-O2': 'P4', 
+    'FZ-CZ': 'Fz', 'CZ-PZ': 'Cz', 'FP2-F8': 'P5', 'F8-T8': 'F8', 
+    'T8-P8-0': 'T8', 'P8-O2': 'P8', 'FP1-F3': 'P2', 'P7-T7': 'P9',
+    'T7-FT9': 'T9', 'FT9-FT10': 'FT9', 'FT10-T8': 'FT10', 'T8-P8-1': 'TP8'
+}
